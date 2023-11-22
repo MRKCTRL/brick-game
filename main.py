8b041900baac3e142f24ce7c467428dc0f814373
MRKCTRL <davidmbundu@gmail.com>
2023-11-22 13:22:04 +0200

    added functions and paddle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from turtle import Screen
+from turtle import Screen, Turtle
 import random
 
 # width 800
@@ -9,12 +9,38 @@ screen = Screen()
 screen.bgcolor('black')
 screen.setup(height=600,width=800)
 screen.title('Brick-Game')
+screen.tracer(0)
+
+paddle = Turtle()
+paddle.shape('sqaure')
+paddle.color('grey')
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.penup()
+paddle.goto(350,0)
+
+
+screen.listen()
+screen.onkey(go_left, 'Left')
+screen.onkey(go_right, 'Right')
+
+game_on = True
+while game_on:
+    screen.update()
+
 
 difficulty_level = input('what is your leve of difficulty? Easy, Normal or Hard: ')
 
 players = input('How many players will be playing? ')
 
 color_choice = input('What is the color of your board Red or Blue? ')
+
+def go_left():
+    new_y paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+def go_right():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
 
 
 def power_up():
