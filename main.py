2e9d1f0415308294ff0758df72db310ac09b55c6
MRKCTRL <davidmbundu@gmail.com>
2023-11-22 12:45:01 +0200

    start only functions, no gui or much code

---
+++ main.py
@@ -0,0 +1,45 @@
+difficulty_level = input('what is your leve of difficulty? Easy, Normal or Hard: ')
+
+players = input('How many players will be playing? ')
+
+color_choice = input('What is the color of your board Red or Blue? ')
+
+
+def power_up():
+# a thingie will come if player takes it, they get a special power, life, larger, or something
+
+
+def timer():
+# how long a player has been playing
+
+ss
+
+
+def score():
+# score for how many times they hit the brick
+
+
+
+def level_up():
+# when score reaches a level, they get a power
+
+
+
+
+def lives():
+# the lives a player has
+
+
+def difficulty_level():
+# hard, easy or normal
+
+
+
+def color_board():
+    # color of the board
+
+
+
+
+def speed():
+# hoq fast the ball comes
