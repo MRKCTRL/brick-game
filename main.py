e0b8b6efa3378ed941bbe2346193f94018b63703
MRKCTRL <davidmbundu@gmail.com>
2023-11-22 15:41:26 +0200

    the main.py got cluttered with so many functions and to make it look clean i create a file that has

--- main.py
+++ main.py
@@ -59,47 +59,4 @@ color_choice = input('What is the color of your board Red or Blue? ')
 
 
 
-def power_up():
-# a thingie will come if player takes it, they get a special power, life, larger, or something
-
-
-def timer():
-# how long a player has been playing
-
-ss
-
-
-def score():
-# score for how many times they hit the brick
-
-
-
-def level_up():
-# when score reaches a level, they get a power
-
-
-
-
-def lives():
-# the lives a player has
-
-
-def difficulty_level():
-# hard, easy or normal
-
-
-
-def color_board():
-    # color of the board
-
-def bonus_points():
-
-
-
-
-
-def speed():
-# hoq fast the ball comes
-
-
 screen.exitonclick()
---
+++ powers.py
@@ -0,0 +1,43 @@
+from turtle import Turtle
+
+
+class Power(Turtle):
+
+    def __init__(self, position):
+        super().__init__()
+
+    def power_up():
+    # a thingie will come if player takes it, they get a special power, life, larger, or something
+
+
+    def timer():
+    # how long a player has been playing
+
+
+
+    def score():
+    # score for how many times they hit the brick
+
+
+    def level_up():
+    # when score reaches a level, they get a power
+
+
+
+    def lives():
+    # the lives a player has
+
+
+    def difficulty_level():
+    # hard, easy or normal
+
+
+    def color_board():
+        # color of the board
+
+    def bonus_points():
+
+
+
+    def speed():
+    # hoq fast the ball comes
